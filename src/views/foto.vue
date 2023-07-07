ef8d295b47aa162724e89244fcf713d3e04c370f
ahmadfarohi <ahmadfarohi@student.uir.ac.id>
2023-07-07 20:50:38 +0700

    first commit

---
+++ src/views/foto.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="photo-widget">
+        <h1 class="ket">Favorit Person</h1>
+      <div class="image-container">
+        <img :src="currentPhoto" :alt="currentPhoto" :style="{ width: '1000px', height: 'auto' }" />
+      </div>
+      <div class="controls-foto">
+        <button @click="BackToHome">Back To Home</button>
+        <button @click="prevPhoto">Previous</button>
+        <button @click="nextPhoto">Next</button>
+      </div>
+    </div>
+  </template>
+  
+  
+  <script>
+  export default {
+    data() {
+      return {
+        photos: [require('../assets/dream1.jpeg'),require('../assets/dream2.jpeg'),require('../assets/dream3.jpeg'),require('../assets/dream4.jpeg'),require('../assets/dream5.jpeg'),require('../assets/dream6.jpeg'),require('../assets/dream7.jpeg'),require('../assets//dream8.jpeg'),require('../assets/dream9.jpeg'),require('../assets/dream10.jpeg')],
+        currentIndex: 0,
+      };
+    },
+    computed: {
+      currentPhoto() {
+        return this.photos[this.currentIndex];
+      },
+    },
+    methods: {
+      BackToHome() {
+        this.$router.push('/home')
+      },
+      prevPhoto() {
+        if (this.currentIndex > 0) {
+          this.currentIndex--;
+        } else {
+          this.currentIndex = this.photos.length - 1;
+        }
+      },
+      nextPhoto() {
+        if (this.currentIndex < this.photos.length - 1) {
+          this.currentIndex++;
+        } else {
+          this.currentIndex = 0;
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style>
+.ket {
+    font-weight: bold;
+    margin-bottom: 50px;
+}
+
+.photo-widget {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 110vh;
+}
+
+.image-container {
+  margin-bottom: 20px;
+}
+
+.controls-foto button {
+  padding: 10px 20px;
+  font-size: 18px;
+  background-color: black;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  border-radius: 50px;
+  margin-right: 10px;
+}
+
+.controls-foto button:hover {
+  background-color: beige
+}
+
+  </style>
---
+++ src/views/home.vue
@@ -0,0 +1,82 @@
+<template>
+    <div class="homepage">
+      <h1>Selamat Datang di Halaman Home</h1>
+      <p>Ini adalah halaman utama Vue.js</p>
+      <div class="button-f">
+        <button class="button" @click="masukKeCuaca">Cuaca</button>
+        <button class="button" @click="MasukKeLokasi">Lokasi</button>
+        <button class="button" @click="masukKeStopwatch">Stopwatch</button>
+        <button class="button" @click="masukKeTikTakToe">Tic Tac Toe</button>
+        <button class="button" @click="masukKeFoto">Foto</button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import HelloWorld from '../components/HelloWorld.vue'
+  
+  export default {
+    components: {
+      HelloWorld
+    },
+  
+  methods: {
+      masukKeCuaca() {
+        this.$router.push('/cuaca')
+      },
+      masukKeStopwatch() {
+        this.$router.push('/stopwatch')
+      },
+      MasukKeLokasi() {
+        this.$router.push('/lokasi')
+      },
+      masukKeFoto() {
+        this.$router.push('/foto')
+      },
+      masukKeTikTakToe() {
+        this.$router.push('/tictactoe')
+      },
+    }
+  }
+  </script>
+
+<style>
+.homepage {
+    background-image: url('../assets/backgrond.jpeg');
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+
+.button-f {
+  margin-top: 20px;
+}
+
+.button, .custom-button {
+  display: inline-block;
+  margin: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 5px;
+  background-color: palevioletred;
+  color: peachpuff;
+  border: none;
+  cursor: pointer;
+}
+
+.button:hover, .custom-button:hover {
+  background-color: pink;
+}
+
+.custom-button {
+  background-color: plum;
+}
+
+.custom-button:hover {
+  background-color: plum;
+}
+</style>
